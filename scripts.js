8623411cfabd9c0d3e3bab40f56d73929d0c8773
SUJAN DAS <dassujan.005@gmail.com>
2022-09-12 23:58:29 +0530

    Intial commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+// alert('linked')
+
 /* Creating the 'X' close symbol in each of the LI TAGS  */
+
 var myNodeItem = document.getElementsByTagName('li');
 
 var i;
@@ -15,6 +18,7 @@ for (i = 0; i < myNodeItem.length; i++) {
 }
 
 /* Making the 'X' close symbol to be able to delete the LI TAGS Lists */
+
 var close = document.getElementsByClassName('close');
 
 var i;
@@ -30,6 +34,7 @@ for (i = 0; i < close.length; i++) {
 }
 
 /* Adding a check mark and a strike through each of the 'LI' Tags*/
+
 var list = document.querySelector('ul');
 
 list.addEventListener('click',
@@ -43,6 +48,7 @@ list.addEventListener('click',
     false)
 
 /* Getting the input tag to display a message if we type a blank message */
+
 function newElement() {
 
     var li = document.createElement('li');
@@ -54,7 +60,7 @@ function newElement() {
     li.appendChild(t);
 
     if (inputValue === '') {
-        alert('please write some text here....')
+        alert('Please write something about your task here...')
     } else {
         document.getElementById('muUL').appendChild(li);
     }
@@ -71,12 +77,12 @@ function newElement() {
     for (i = 0; i < close.length; i++) {
 
         close[i].onclick = function () {
-    
+
             var div = this.parentElement;
             div.style.display = 'none';
-    
+
         }
-    
+
     }
 
 }
